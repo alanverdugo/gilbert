6952b6c8a881a6bf63aca3fa90451a38cc043e99
Alan Verdugo <factorpolar@protonmail.com>
2020-03-22 21:50:11 -0600

    Refactoring and adding icons

--- main.py
+++ main.py
@@ -2,14 +2,8 @@
 """A mobile application that helps learn about electricity and magnetism."""
 
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-#from kivy.uix.boxlayout import BoxLayout
-#from kivy.uix.widget import Widget
-from kivy.uix.popup import Popup
 from kivy.logger import Logger
-#from kivy.lang import Builder
 from kivy.uix.settings import SettingsWithTabbedPanel
 from kivy.uix.screenmanager import ScreenManager, Screen
 
@@ -24,6 +18,14 @@ class MenuScreen(Screen):
 class MenuScreenButton(Button):
     """
     Custom button style for the Menu Screen.
+
+    It inherits from kivy.uix.button.
+    """
+    pass
+
+class BackToMenuButton(Button):
+    """
+    Custom button style to go back to the Menu Screen.
 
     It inherits from kivy.uix.button.
     """
@@ -55,6 +57,13 @@ class QuizScreen(Screen):
     """
     pass
 
+class AboutScreen(Screen):
+    """
+    Custom Screen Class for the About section.
+
+    It inherits from Screen.
+    """
+    pass
 
 CONFIG = '''
 [
@@ -92,6 +101,7 @@ class Gilbert(App):
         self.screen_manager.add_widget(MenuScreen(name="menu_screen"))
         self.screen_manager.add_widget(StudyScreen(name="study_screen"))
         self.screen_manager.add_widget(QuizScreen(name="quiz_screen"))
+        self.screen_manager.add_widget(AboutScreen(name="about_screen"))
 
         # We apply the saved configuration settings or the defaults
         #root = Builder.load_string(kv)
@@ -99,6 +109,7 @@ class Gilbert(App):
         #label.text = self.config.get('My Label', 'text')
         #MenuScreenButton.font_size = float(self.config.get('MenuScreenButton', 'font_size'))
         #return root
+
         return self.screen_manager
 
     def build_config(self, config):
@@ -134,59 +145,6 @@ class Gilbert(App):
         """
         Logger.info("main.py: App.close_settings: %s", settings)
         super(Gilbert, self).close_settings(settings)
-
-"""
-class MenuScreen(BoxLayout):
-    ""Main menu screen.""
-
-    def __init__(self, **kwargs):
-        #super(MenuScreen, self).__init__(**kwargs)
-
-        # Create one of our custom "Menu-screen buttons".
-        button1 = MenuScreenButton(text="Study")
-        #button1.bind(on_press=self.callback)
-        self.add_widget(button1)
-        button1.bind(on_press=self.open_study_section)
-
-        # Create one of our custom "Menu-screen buttons".
-        button2 = MenuScreenButton(text="Quiz")
-        #button2.bind(on_press=self.callback)
-        self.add_widget(button2)
-        button2.bind(on_press=open_quiz_section)
-
-        # Create our custom "Settings button".
-        button3 = SettingsButton()
-        self.add_widget(button3)
-
-    def open_study_section(self, event):
-        #StudyScreen.manager.transition.direction = 'left'
-        #StudyScreen.manager.transition.duration = 1
-        #StudyScreen.manager.current = 'MenuScreen'
-        print(self.screen_manager.next())
-        self.screen_manager.transition.direction = 'left'
-        self.screen_manager.transition.duration = 1
-        self.screen_manager.current = self.screen_manager.next()
-        #self.screen_manager.current = "study_screen"
-"""
-
-def open_quiz_section(event):
-    layout = GridLayout(cols=1, padding=10)
-
-    popup_label = Label(text="Quiz section")
-    close_button = Button(text="Close")
-
-    layout.add_widget(popup_label)
-    layout.add_widget(close_button)
-
-    # Instantiate the modal popup and display.
-    popup = Popup(title='Redirecting to...',
-                  content=layout,
-                  size_hint=(None, None),
-                  size=(400, 400))
-    popup.open()
-
-    # Attach close button press with popup.dismiss action.
-    close_button.bind(on_press=popup.dismiss)
 
 
 if __name__ == "__main__":
